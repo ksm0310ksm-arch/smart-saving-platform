d3826b3c6a819fd3b66c9dd9c7cec684ee1a41dc
neungwoo <skasmddn9175@gmail.com>
2025-11-05 18:52:08 +0900

    11-12 Saving 업로드 - 능우

--- src/main/java/com/example/smart/SmartApplication.java
+++ src/main/java/com/example/smart/SmartApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = "com.example") // ✅ 전체 com.example 패키지 스캔
-@EnableJpaRepositories(basePackages = "com.example.repository") // ✅ repository 인식
-@EntityScan(basePackages = "com.example.entity") // ✅ entity 인식
+@SpringBootApplication(scanBasePackages = "com.example.smart") // ✅ smart 이하 전부 스캔
+@EnableJpaRepositories(basePackages = "com.example.smart.repository") // ✅ repository 인식
+@EntityScan(basePackages = "com.example.smart.entity") // ✅ entity 인식
 public class SmartApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmartApplication.class, args);
--- src/main/java/com/example/smart/config/WebConfig.java
+++ src/main/java/com/example/smart/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.example.config;
+package com.example.smart.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
